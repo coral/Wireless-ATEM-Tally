7003d5b1dc5372008e88faa96c5a85667d1d1719
Jonas Bengtson <jonas@jonasbengtson.se>
2013-02-06 11:06:50 +0100

    Added client project file

---
+++ Client/Client.ino
@@ -0,0 +1,20 @@
+/*****************
+ * Wirless Tally for ATEM
+ * Client file
+ * authors: coral
+ * library and example: kasper skaarhoj
+ */
+
+//Define the LED pins
+int RED = 7;
+int GREEN = 6;
+
+void setup() {
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  //fix tally here 
+}
